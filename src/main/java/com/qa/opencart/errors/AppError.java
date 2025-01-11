2299e4328bf885aa8d8d1eda6967eac249ffba3c
dib1987 <43892634+dib1987@users.noreply.github.com>
2025-01-11 17:48:21 +0530

    small changes

--- src/main/java/com/qa/opencart/errors/AppError.java
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -3,6 +3,6 @@ package com.qa.opencart.errors;
 public class AppError {
 	
 	
-	public static final String INVALID_BROWSER_MSG="Please pass the right browser name...";
+	public static final String INVALID_BROWSER_MSG="Please pass the right browser name..";
 
 }
